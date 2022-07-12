4e8fbcbcf01de3d019ad3d01749da0436c079444
Milan Assuied <milan.assuied@unity3d.com>
2022-07-12 11:05:16 -0400

    Initial commit

---
+++ ConveyorBelt/Assets/Scripts/Controller.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using Mechatronics.SystemGraph;
+
+
+[NodeCategory("conveyor", "controller", NodeTick.Asynchronous)]
+public class Controller : NodeRuntime
+{
+	[Field("EmergencyStop", PortDirection.Left, FieldExtra.Read), SerializeField]
+	private PortType<bool> emergencyStop = new();
+
+	[Field("ItemIn", PortDirection.Left, FieldExtra.Read), SerializeField]
+	private PortType<bool> itemIn = new();
+
+	[Field("ItemOut", PortDirection.Left, FieldExtra.Read), SerializeField]
+	private PortType<bool> itemOut = new();
+	
+	[Field("IsRunning", PortDirection.Right, FieldExtra.Write), SerializeField]
+	private PortType<bool> isRunning = new();
+	
+	[Field("CurrentNumberOfItems", PortDirection.Right, FieldExtra.Write), SerializeField]
+	private PortType<uint> currentNumberOfItems = new();
+	
+	[Field("TotalNumberOfItems", PortDirection.Right, FieldExtra.Write), SerializeField]
+	private PortType<uint> totalNumberOfItems = new();
+
+	private uint m_CurrentItemCount;
+	private uint m_TotalItemCount;
+	
+	public override void Enable(Scheduler.ClockState clockState)
+	{
+		m_CurrentItemCount = 0;
+		m_TotalItemCount = 0;
+		
+		itemIn.ChangeEvent += OnItemIn;
+		itemOut.ChangeEvent += OnItemOut;
+		
+		itemIn.ChangeEvent += ProcessOutputs;
+		itemOut.ChangeEvent += ProcessOutputs;
+
+		emergencyStop.ChangeEvent += ProcessOutputs;
+		
+		ProcessOutputs();
+	}
+
+	public override void Disable()
+	{
+		itemIn.ChangeEvent -= OnItemIn;
+		itemOut.ChangeEvent -= OnItemOut;
+		
+		itemIn.ChangeEvent -= ProcessOutputs;
+		itemOut.ChangeEvent -= ProcessOutputs;
+
+		emergencyStop.ChangeEvent -= ProcessOutputs;
+	}
+
+	private void OnItemIn()
+	{
+		if (itemIn.Read)
+		{
+			m_CurrentItemCount++;
+			m_TotalItemCount++;
+		}
+	}
+	
+	private void OnItemOut()
+	{
+		if (itemOut.Read)
+		{
+			m_CurrentItemCount--;
+		}
+	}
+
+	private void ProcessOutputs()
+	{
+		currentNumberOfItems.Write = m_CurrentItemCount;
+		totalNumberOfItems.Write = m_TotalItemCount;
+		
+		isRunning.Write = !emergencyStop.Read && m_CurrentItemCount > 0;
+	}
+}
