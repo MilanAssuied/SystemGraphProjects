5f794c493982bdcb9d1d1d88c3dcf55651f9f66d
Vincent Zalzal <vincent.zalzal@unity3d.com>
2022-07-22 09:22:45 -0400

    Add event delegates to collision sensor and toggle button

--- ConveyorBelt/Assets/Scripts/CollisionSensor.cs
+++ ConveyorBelt/Assets/Scripts/CollisionSensor.cs
@@ -1,21 +1,21 @@
+using System;
 using UnityEngine;
 
 public class CollisionSensor : MonoBehaviour
 {
-    [SerializeField]
-    private bool m_CollisionDetected;
+    public event Action StateChanged;
 
-    public bool collisionDetected => m_CollisionDetected;
+    public bool CollisionDetected { get; private set; }
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collision detected");
-        m_CollisionDetected = true;
+        CollisionDetected = true;
+        StateChanged?.Invoke();
     }
     
     public void OnTriggerExit(Collider other)
     {
-        Debug.Log("Collision exited");
-        m_CollisionDetected = false;
+        CollisionDetected = false;
+        StateChanged?.Invoke();
     }
 }
--- ConveyorBelt/Assets/Scripts/ToggleButton.cs
+++ ConveyorBelt/Assets/Scripts/ToggleButton.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ToggleButton : MonoBehaviour
@@ -5,11 +6,14 @@ public class ToggleButton : MonoBehaviour
     [SerializeField]
     private float _travelDistance = 0.02f;
 
+    public event Action StateChanged;
+
     public bool Pushed { get; private set; }
 
     private void OnMouseDown()
     {
         transform.Translate(_travelDistance * (Pushed ? Vector3.up : Vector3.down), Space.Self);
         Pushed = !Pushed;
+        StateChanged?.Invoke();
     }
 }
